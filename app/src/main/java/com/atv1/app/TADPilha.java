7c478be37ff1e5fee715528c0fe3fe01105e30c7
AbraaoAlves <abraao.teodosio@gmail.com>
2024-04-27 20:24:31 +0000

    feat: add tad pilha

---
+++ app/src/main/java/com/atv1/app/TADPilha.java
@@ -0,0 +1,17 @@
+package com.atv1.app;
+
+public interface TADPilha {
+    
+  // Adds an element to the end of the stack.
+  void push(int num); 
+  // Removes the topmost element from the stack.
+  int pop(); 
+  // Retrieves the topmost element without removing it.
+  int peek();
+
+  // Checks if the stack is empty.
+  boolean isEmpty();
+  
+  // Checks if the stack is full.
+  boolean isFull();
+}
