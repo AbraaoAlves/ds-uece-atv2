2a679c3857800f35c182d9c01a1e2326a3e7a6a6
AbraaoAlves <abraao.teodosio@gmail.com>
2024-04-27 20:26:19 +0000

    feat: add question 1 file

---
+++ app/src/main/java/com/atv1/app/Question1.java
@@ -0,0 +1,16 @@
+package com.atv1.app;
+
+
+/**
+ * 
+ *  Utilizando apenas o TAD Pilha, proponha um algoritmo que 
+ *  armazene 10 valores inteiros, gerados aleatoriamente 
+ *  no intervalo de 1 a 100, em uma pilha P. 
+ * 
+ * Em seguida, inverta a ordem dos elementos na pilha P.
+ */
+public class Question1 {
+    public static void main(String[] args) {
+      // TADPilha pilha = new Pilha(10);
+    }
+}
