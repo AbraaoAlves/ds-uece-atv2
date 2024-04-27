afc01b8d1275528a953e48ce572c2c26428bfc03
AbraaoAlves <abraao.teodosio@gmail.com>
2024-04-27 13:14:19 +0000

    chore: remove unnecessary code

--- app/src/main/java/com/atv1/app/AppApplication.java
+++ app/src/main/java/com/atv1/app/AppApplication.java
@@ -1,17 +1,8 @@
 package com.atv1.app;
 
-// import java.util.ArrayList;
-// import java.util.List;
-
 public class AppApplication {
 
 	public static void main(String[] args) {
-		int n = 0;
-
-		while(n < 5) {
-			System.out.println("bom dia");
-			n = n +1;
-		}
 		System.out.println("fim");
 	
 	}
