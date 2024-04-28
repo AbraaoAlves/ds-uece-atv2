63405ff075e5095e96019f817ee5bdfc0f3d4170
AbraaoAlves <abraao.teodosio@gmail.com>
2024-04-28 11:38:51 +0000

    chore: add Question4 file

---
+++ app/src/main/java/com/atv1/app/Question4.java
@@ -0,0 +1,11 @@
+package com.atv1.app;
+
+
+/**
+ *  Considere uma pilha P vazia e uma fila F não vazia. Utilizando apenas as funções desses
+ TADs, elabore um algoritmo que inverta a ordem dos elementos na fila F.
+ */
+public class Question4 {
+  
+}
+  
