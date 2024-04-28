c877e72b34e028b358c9fd86847af2462e6b2b11
AbraaoAlves <abraao.teodosio@gmail.com>
2024-04-28 14:03:06 +0000

    feat: solve question4

--- app/src/main/java/com/atv2/app/Question4.java
+++ app/src/main/java/com/atv2/app/Question4.java
@@ -1,10 +1,30 @@
 package com.atv2.app;
 
+import com.atv2.app.lib.Fila;
+import com.atv2.app.lib.Pilha;
 
 /**
  *  Considere uma pilha P vazia e uma fila F não vazia. Utilizando apenas as funções desses
  TADs, elabore um algoritmo que inverta a ordem dos elementos na fila F.
  */
 public class Question4 {
-  
-}
+  public static void main(String[] args) {
+    Pilha pilha = new Pilha(10);
+    Fila fila = new Fila(10);
+    
+    for (int i = 0; i < 10; i++) {
+      fila.enqueue(i);
+    }
+    
+    System.out.println("Pilha: " + pilha.toString());
+    System.out.println("Fila: " + fila.toString());
+    System.out.println("Invertendo a fila na pilha...");
+
+    while (!fila.isEmpty()) {
+      pilha.push(fila.dequeue());
+    }
+    
+    System.out.println("Pilha: " + pilha.toString());
+    System.out.println("Fila: " + fila.toString());
+  }
+} 
